ab216f82bd97b4b66eb13bcf7ebeac04df0c3a3d
Martin Benonisen <mbenonisen@gmail.com>
2017-09-13 00:51:06 +0000

    updated gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,14 @@ module.exports = function(grunt) {
                 ptr: false
             },
             dist: {
-                src: ['dist/*.js']
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'dist/',
+                        src: ['**/*.js'],
+                        flatten: true
+                    }
+                ]
             }
         }
     });
